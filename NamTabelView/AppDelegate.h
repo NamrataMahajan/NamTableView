470eacf520703bb1bc399585c75d10e444714d4d
Namrata <namrata@Namratas-MacBook-Air.local>
2016-09-19 11:11:15 +0530

    Initial Commit

---
+++ NamTabelView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NamTabelView
+//
+//  Created by Namrata on 19/09/16.
+//  Copyright © 2016 Namrata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
